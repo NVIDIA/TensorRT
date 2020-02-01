7ccf312c82ec8af74b53113120a597ce3915f3a6
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-01 21:59:42 +0800

    unify exit(1) to exit(EXIT_FAILURE) and unify exit(0) to exit(EXIT_SUCCESS)
    Signed-off-by: keineahnung2345 <mimifasosofamire1123@gmail.com>

--- samples/common/common.h
+++ samples/common/common.h
@@ -355,7 +355,7 @@ inline void* safeCudaMalloc(size_t memSize)
     if (deviceMem == nullptr)
     {
         std::cerr << "Out of memory" << std::endl;
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     return deviceMem;
 }
