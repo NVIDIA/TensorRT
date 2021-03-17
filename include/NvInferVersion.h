853d3311e8a3f5e09890e54167972633de705ad2
Rajeev Rao <rajeevrao@nvidia.com>
2021-03-16 23:07:39 -0700

    Update TensorRT headers to 7.2.3.4
    Signed-off-by: Rajeev Rao <rajeevrao@nvidia.com>

--- include/NvInferVersion.h
+++ include/NvInferVersion.h
@@ -25,11 +25,11 @@
 
 #define NV_TENSORRT_MAJOR 7 //!< TensorRT major version.
 #define NV_TENSORRT_MINOR 2 //!< TensorRT minor version.
-#define NV_TENSORRT_PATCH 2 //!< TensorRT patch version.
-#define NV_TENSORRT_BUILD 3 //!< TensorRT build number.
+#define NV_TENSORRT_PATCH 3 //!< TensorRT patch version.
+#define NV_TENSORRT_BUILD 4 //!< TensorRT build number.
 
 #define NV_TENSORRT_SONAME_MAJOR 7 //!< Shared object library major version number.
 #define NV_TENSORRT_SONAME_MINOR 2 //!< Shared object library minor version number.
-#define NV_TENSORRT_SONAME_PATCH 2 //!< Shared object library patch version number.
+#define NV_TENSORRT_SONAME_PATCH 3 //!< Shared object library patch version number.
 
 #endif // NV_INFER_VERSION_H
