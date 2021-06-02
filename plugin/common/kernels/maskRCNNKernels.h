602c116065fadcc7d2bdbc9ef04be2859fbbed3d
Tyler Zhu <tylerz@nvidia.com>
2021-06-02 16:37:47 +0800

    Optimize TLT MaskRCNN plugins
    - enable fp16 precision in multilevelCropAndResizePlugin and multilevelProposeROIPlugin
    - Algorithms optimization for NMS kernels and ROIAlign kernel
    - Fix invalid cuda config issue when bs is larger than 32
    - Fix issues found on Jetson NANO
    
    Signed-off-by: Tyler Zhu <tylerz@nvidia.com>

--- plugin/common/kernels/maskRCNNKernels.h
+++ plugin/common/kernels/maskRCNNKernels.h
@@ -244,7 +244,9 @@ cudaError_t DecodeBBoxes(cudaStream_t stream, int N,
     const float* regWeight, const float inputHeight, const float inputWidth,
     const void* anchors, // [N, anchors, (y1, x1, y2, x2)]
     const void* delta,   //[N, anchors, (dy, dx, log(dh), log(dw)]
-    void* outputBbox);
+    void* outputBbox,
+    nvinfer1::DataType dtype
+    );
 
 cudaError_t ApplyDelta2Bboxes(cudaStream_t stream, int N,
     int samples,         // number of anchors per image
@@ -279,7 +281,7 @@ cudaError_t roiAlignHalfCenter(cudaStream_t stream, int batchSize, int featureCo
 
     int inputHeight, int inputWidth, const void* rois, const void* const layers[], const xy_t* layerDims,
 
-    void* pooled, const xy_t poolDims);
+    void* pooled, const xy_t poolDims, const DataType dtype);
 
 // RESIZE NEAREST
 void resizeNearest(dim3 grid, dim3 block, cudaStream_t stream, int nbatch, float scale, int2 osize, float const* idata,
