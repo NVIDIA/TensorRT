595f0bb091aafacab7b78e710f0452b893af910e
Rajeev Rao <rajeevrao@nvidia.com>
2021-08-23 16:08:01 +0000

    Update Tacotron2 demo for TensorRT 8.0
    Signed-off-by: Rajeev Rao <rajeevrao@nvidia.com>

--- demo/Tacotron2/scripts/inference_benchmark.sh
+++ demo/Tacotron2/scripts/inference_benchmark.sh
@@ -16,6 +16,6 @@
 #
 
 echo "TensorRT BS=1, S=128"
-bash test_infer.sh --test trt/test_infer_trt.py -bs 1 -il 128 --fp16 --num-iters 1003 --encoder ./output/encoder_fp16.engine --decoder ./output/decoder_iter_fp16.engine --postnet ./output/postnet_fp16.engine --waveglow ./output/waveglow_fp16.engine --wn-channels 256
+bash test_infer.sh --test tensorrt/test_infer_trt.py -bs 1 -il 128 --fp16 --num-iters 1003 --encoder ./output/encoder_fp16.engine --decoder ./output/decoder_with_outer_loop_fp16.engine --postnet ./output/postnet_fp16.engine --waveglow ./output/waveglow_fp16.engine --wn-channels 256
 echo "PyTorch (GPU) BS=1, S=128"
 bash test_infer.sh -bs 1 -il 128 --fp16 --num-iters 1003 --tacotron2 ./checkpoints/tacotron2_pyt_ckpt_amp_v19.09.0/nvidia_tacotron2pyt_fp16_20190427 --waveglow ./checkpoints/waveglow_ckpt_amp_256_v19.10.0/nvidia_waveglow256pyt_fp16 --wn-channels 256
