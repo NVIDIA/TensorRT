052281f0ab795b6c1a19047dc8a449cd397995a9
Rajeev Rao <rajeevrao@nvidia.com>
2022-04-11 13:34:07 -0700

    Update copyright headers with SPDX identifiers
    Signed-off-by: Rajeev Rao <rajeevrao@nvidia.com>

--- samples/sampleOnnxMnistCoordConvAC/modify_onnx_ac.py
+++ samples/sampleOnnxMnistCoordConvAC/modify_onnx_ac.py
@@ -1,11 +1,12 @@
-##########################################################################
-# Copyright (c) 2018-2019 NVIDIA Corporation. All rights reserved.
+#
+# SPDX-FileCopyrightText: Copyright (c) 1993-2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,11 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# https://github.com/onnx/onnx/issues/2259
-# onnx-graphsurgeon
-##########################################################################
-
-
 
 import torch
 import onnx
--- scripts/copyright-scan.py
+++ scripts/copyright-scan.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
-# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 1993-2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
