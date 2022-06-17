bd488c75de56dfcc9f169f61ec47d6e5e9f3d40c
Gwena Cunha <gwena.cs@gmail.com>
2022-06-17 12:28:38 +0900

    Updated installation instructions with public links
    Signed-off-by: Gwena Cunha <gwena.cs@gmail.com>

--- tools/tensorflow-quantization/install.sh
+++ tools/tensorflow-quantization/install.sh
@@ -3,4 +3,6 @@
 pip install nvidia-pyindex
 pip install onnx-graphsurgeon
 pip install git+https://github.com/onnx/tensorflow-onnx.git
+pip install tensorflow-gpu==2.8.0 tensorflow-datasets
+pip install numpy pytest pytest-html graphviz
 pip install .
