Expanded model zoo + fixed links in TF-QAT toolkit (w/ sign-off)
- Added Inception-v3 support: code and results
- Fixed broken links
- Fix MobileNet test case

Signed-off-by: Gwena Cunha <gwena.cs@gmail.com>

tools/tensorflow-quantization/examples/data/data_loader.py
@@ -57,6 +57,7 @@ _SUPPORTED_MODEL_NAMES = [
     "efficientnet_b3",
     "mobilenet_v1",
     "mobilenet_v2",
+    "inception_v3",
 ]
 
 _NUM_CLASSES = 1000
@@ -68,20 +69,23 @@ _NUM_IMAGES = {
 _DEFAULT_IMAGE_SIZE = {
     "resnet_v1": 224,
     "resnet_v2": 299,
-    "mobilenet_v1": 224,
-    "mobilenet_v2": 224,
     "efficientnet_b0": 224,
     "efficientnet_b3": 300,
+    "mobilenet_v1": 224,
+    "mobilenet_v2": 224,
+    "inception_v3": 299,
 }
 _NUM_CHANNELS = 3
 _RESIZE_MIN = {
     "resnet_v1": 256,
     "resnet_v2": 342,
-    "mobilenet_v1": 256,
-    "mobilenet_v2": 256,
     "efficientnet_b0": 256,
     "efficientnet_b3": 342,
+    "mobilenet_v1": 256,
+    "mobilenet_v2": 256,
+    "inception_v3": 342,
 }
+
 
 def load_image_np(test_image, model_name: str = "resnet_v1"):
     # Image is loaded in NHWC format
@@ -91,6 +95,7 @@ def load_image_np(test_image, model_name: str = "resnet_v1"):
     image = _central_crop(image, _DEFAULT_IMAGE_SIZE[model_name], _DEFAULT_IMAGE_SIZE[model_name])
     image = preprocess_model_func(image, model_name)
     return image
+
 
 def get_filenames(
     data_dir: str,
@@ -226,6 +231,7 @@ def preprocess_image_record(record, min_size=256, image_height=224, image_width=
 
     return image, label
 
+
 def preprocess_model_func(image: tf.Tensor, model_name: str = "resnet_v1"):
 
     if model_name == "resnet_v1":
@@ -236,11 +242,14 @@ def preprocess_model_func(image: tf.Tensor, model_name: str = "resnet_v1"):
         return tf.keras.applications.mobilenet.preprocess_input(image)
     elif model_name == "mobilenet_v2":
         return tf.keras.applications.mobilenet_v2.preprocess_input(image)
+    elif model_name == "inception_v3":
+        return tf.keras.applications.inception_v3.preprocess_input(image)
     else:
         # efficientnet doesn't need specific pre-processing (included in the model itself).
         print("No further pre-processing found for {}".format(model_name))
 
     return image
+
 
 def load_data_tfrecord_tf(
     data_dir: str = "./data/imagenet",

tools/tensorflow-quantization/examples/inception/test_qdq_node_placement.py
@@ -18,48 +18,43 @@
 
 import sys
 import tensorflow as tf
-from tensorflow_quantization.custom_qdq_cases import MobileNetQDQCase
+from tensorflow_quantization.custom_qdq_cases import InceptionQDQCase
 from examples.utils import get_tfkeras_model
 from tests.onnx_graph_qdq_validator import validate_quantized_model
 from tensorflow_quantization.utils import CreateAssetsFolders
+from tensorflow_quantization.quantize import LayerConfig
 import pytest
 
 # Create a directory to save test models
 test_assets = CreateAssetsFolders("test_qdq_node_placement")
 
-
-def test_mobilenetv1_quantize_full():
-    """
-    MobileNet-v1: Full model quantization
-    """
-    this_function_name = sys._getframe().f_code.co_name
-
-    # Instantiate Baseline model
-    nn_model_original = get_tfkeras_model(model_name="mobilenet_v1")
-
-    custom_qdq_cases = [MobileNetQDQCase()]
-    q_model, validated = validate_quantized_model(
-        test_assets, nn_model_original, test_name=this_function_name,
-        custom_qdq_cases=custom_qdq_cases
+EXPECTED_QDQ_INSERTION = [
+    LayerConfig(name="Conv2D", is_keras_class=True),
+    LayerConfig(name="Dense", is_keras_class=True),
+    LayerConfig(name="DepthwiseConv2D", is_keras_class=True),
+    LayerConfig(
+        name="AveragePooling2D", is_keras_class=True, quantize_weight=False
+    ),
+    LayerConfig(
+        name="GlobalAveragePooling2D", is_keras_class=True, quantize_weight=False
     )
-    assert validated, "ONNX QDQ validation for full network quantization failed!"
-    # necessary to clear model layer names from the memory
-    tf.keras.backend.clear_session()
+]
 
 
-def test_mobilenetv2_quantize_full():
+def test_inceptionv3_quantize_full():
     """
-    MobileNet-v2: Full model quantization
+    Inception-v3: Full model quantization
     """
     this_function_name = sys._getframe().f_code.co_name
 
     # Instantiate Baseline model
-    nn_model_original = get_tfkeras_model(model_name="mobilenet_v2")
+    nn_model_original = get_tfkeras_model(model_name="inception_v3")
 
-    custom_qdq_cases = [MobileNetQDQCase()]
+    custom_qdq_cases = [InceptionQDQCase()]
     q_model, validated = validate_quantized_model(
         test_assets, nn_model_original, test_name=this_function_name,
-        custom_qdq_cases=custom_qdq_cases
+        custom_qdq_cases=custom_qdq_cases,
+        expected_qdq_insertion=EXPECTED_QDQ_INSERTION
     )
     assert validated, "ONNX QDQ validation for full network quantization failed!"
     # necessary to clear model layer names from the memory